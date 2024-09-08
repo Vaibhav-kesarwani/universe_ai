3a4d4c77747eb1f725488a4cb1e6f5fab61584ec
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-09-08 15:38:47 +0530

    Importing the splash screen in the app.dart

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:universe_ai/screens/splash_screen/splash_screen.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Scaffold(),
+      home: const SplashScreen(),
     );
   }
 }
