619a7c12cf1bc8b34eba03216a09b647298e80f6
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-09-08 15:21:20 +0530

    Fix : MAke the fullscreen trigger while the splash screen apperas

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:universe_ai/app/app.dart';
 
 void main() {
-  runApp (const MyApp());
+  // To trigger the fullscreen while splash screen is appearing
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
+  runApp(const MyApp());
 }
