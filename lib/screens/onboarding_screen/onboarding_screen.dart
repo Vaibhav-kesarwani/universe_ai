624e16fe28879d4747b6d42289300e48788ae608
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-09-09 11:30:34 +0530

    Create: Make the first page of the onboarding screen

--- lib/screens/onboarding_screen/onboarding_screen.dart
+++ lib/screens/onboarding_screen/onboarding_screen.dart
@@ -1,10 +1,85 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:universe_ai/helper/global.dart';
 
 class OnboardingScreen extends StatelessWidget {
   const OnboardingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          // Lottie animation
+          Lottie.asset(
+            'assets/onboarding_animation/ai_ask_me.json',
+            height: mq.height * .6,
+          ),
+          // Title
+          const Text(
+            'Ask me Anything',
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w900,
+              letterSpacing: .5,
+            ),
+          ),
+          // Spacing
+          SizedBox(height: mq.height * .01),
+          // Subtitle
+          SizedBox(
+            width: mq.width * .7,
+            child: const Text(
+              'I can be your Best Friend & Your can ask me anything & I will help you!',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 13.5,
+                letterSpacing: .5,
+                color: Colors.black54,
+              ),
+            ),
+          ),
+          // Spacer
+          const Spacer(),
+          // Page Indicator
+          Wrap(
+            spacing: 5,
+            children: List.generate(
+              2,
+              (i) => Container(
+                width: 10,
+                height: 8,
+                decoration: const BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.all(Radius.circular(5)),
+                ),
+              ),
+            ),
+          ),
+          // Spacer
+          const Spacer(),
+          // Button
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.lightBlue,
+              padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+              shape: const StadiumBorder(),
+              elevation: 0,
+              minimumSize: Size(mq.width * .4, 50),
+            ),
+            onPressed: () {},
+            child: const Text(
+              "Next",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+          const Spacer(flex: 2),
+        ],
+      ),
+    );
   }
 }
