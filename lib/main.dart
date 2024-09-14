ff0b6f42c4add5187f28694283419c72df16785f
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-09-14 10:04:23 +0530

    Implement the hive dir

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:universe_ai/app/app.dart';
 
 Future<void> main() async {
   // Make the intial things to exectue before the app runs
   WidgetsFlutterBinding.ensureInitialized();
+
+  // To have the local storage for the app
+  Hive.defaultDirectory = (await getApplicationDocumentsDirectory()).path;
 
   // To trigger the fullscreen while splash screen is appearing
   await SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
