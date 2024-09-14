b3cd38656a42b86794994e2f9086fdffb8d02ddb
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-09-14 10:08:59 +0530

    Export the pref

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:hive/hive.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:universe_ai/app/app.dart';
+import 'package:universe_ai/helper/pref.dart';
 
 Future<void> main() async {
   // Make the intial things to exectue before the app runs
   WidgetsFlutterBinding.ensureInitialized();
 
-  // To have the local storage for the app
-  Hive.defaultDirectory = (await getApplicationDocumentsDirectory()).path;
+  Pref.initalize();
 
   // To trigger the fullscreen while splash screen is appearing
   await SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
-  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  await SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
 
   runApp(const MyApp());
 }
