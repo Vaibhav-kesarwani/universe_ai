503e1c18482886ac8b737d2216b5bedd899b3e9a
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-10-02 10:50:32 +0530

    creatd the home card

--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:universe_ai/helper/global.dart';
 import 'package:universe_ai/helper/pref.dart';
+import 'package:universe_ai/widget/home_card.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -20,9 +22,31 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Home Screen"),
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 1,
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        title: const Text(
+          appName,
+          style: TextStyle(
+              color: Colors.blue, fontSize: 30, fontWeight: FontWeight.bold),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 10),
+            child: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.brightness_4_rounded,
+                  color: Colors.blue,
+                  size: 30,
+                )),
+          )
+        ],
+      ),
+      body: ListView(
+        children: const [HomeCard()],
       ),
     );
   }
---
+++ lib/widget/home_card.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HomeCard extends StatelessWidget {
+  const HomeCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Row(
+        children: [],
+      ),
+    );
+  }
+}
